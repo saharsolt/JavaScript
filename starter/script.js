f2add473af9d2e5dbbff10762f9a977322e21b70
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-05-14 01:10:59 +0300

    Learning Variable and Data Types

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,54 @@
-let js = "Amazing";
-if (js === "Amazing") alert("Javascript is FUN!");
+let js = "amazing";
+if (js === "amazing") alert("JS is FUN!");
 
-40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10);
+console.log(40 + 20 + 10 - 9);
+console.log("Sahar");
+console.log(20);
+
+let firstName = "Sahar";
+let lastName = "Soltanmohammadi";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+//Variable name conventions
+let sahar_soltanmohammadi = "SS";
+let $function = 27;
+
+let person = "Sahar";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let mySecondJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+console.log(myFirstJob);
+
+let country = "Iran";
+let continent = "Asia";
+let population = 80000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let javaScriptISFUN = true;
+console.log(javaScriptISFUN);
+
+console.log(typeof true);
+console.log(typeof javaScriptISFUN);
+//console.log(typeof 23);
+//console.log(typeof "Sahar");
+
+javaScriptISFUN = "YES!";
+console.log(typeof javaScriptISFUN);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2022;
+console.log(year);
+console.log(typeof year);
+
+console.log(null);
