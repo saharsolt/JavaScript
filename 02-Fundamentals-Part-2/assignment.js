4b3a0eb7975168beb02600d71a6481dba2f7e51b
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-05-22 02:38:19 +0300

    Array and Object

--- 02-Fundamentals-Part-2/assignment.js
+++ 02-Fundamentals-Part-2/assignment.js
@@ -71,3 +71,104 @@ function describePopulation(country, population) {
 console.log(describePopulation("Iran", 80));
 console.log(describePopulation("China", 1441));
 console.log(describePopulation("Finland", 5));
+
+/* LECTURE: Introduction to Arrays
+1. Create an array containing 4 population values of 4 countries of your choice.
+You may use the values you have been using previously. Store this array into a
+variable called 'populations'
+2. Log to the console whether the array has 4 elements or not (true or false)
+3. Create an array called 'percentages' containing the percentages of the
+world population for these 4 population values. Use the function
+'percentageOfWorld1' that you created earlier to compute the 4
+percentage values*/
+const populations = new Array(80, 1440, 7, 5);
+console.log(populations.length === 4 ? true : false);
+
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+percentageOfWorld1(populations[0]);
+const percentages = [
+  percentageOfWorld1(populations[0]),
+  percentageOfWorld1(populations[1]),
+  percentageOfWorld1(populations[2]),
+  percentageOfWorld1(populations[populations.length - 1]),
+];
+console.log(percentages);
+
+/*LECTURE: Basic Array Operations (Methods)
+1. Create an array containing all the neighbouring countries of a country of your
+choice. Choose a country which has at least 2 or 3 neighbours. Store the array
+into a variable called 'neighbours'
+2. At some point, a new country called 'Utopia' is created in the neighbourhood of
+your selected country. So add it to the end of the 'neighbours' array
+3. Unfortunately, after some time, the new country is dissolved. So remove it from
+the end of the array
+4. If the 'neighbours' array does not include the country ‘Germany’, log to the
+console: 'Probably not a central European country :D'
+5. Change the name of one of your neighbouring countries. To do that, find the
+index of the country in the 'neighbours' array, and then use that index to
+change the array at that index position. For example, you can search for
+'Sweden' in the array, and then replace it with 'Republic of Sweden'. */
+
+const neighbours = ["Turkey", "Russia", "Iraq", "Afghanistan", "Pakistan", "Torkamanestan"];
+neighbours.push("Utopia");
+neighbours.pop();
+if (!neighbours.includes("Germany")) {
+  console.log("Probably not a central European country :D");
+}
+neighbours[neighbours.indexOf("Torkamanestan")] = "Azarbaijan";
+console.log(neighbours);
+
+/* LECTURE: Introduction to Objects
+1. Create an object called 'myCountry' for a country of your choice, containing
+properties 'country', 'capital', 'language', 'population' and 'neighbours' (an array like we used in previous assignments)*/
+
+const myCountry = {
+  country: "Iran",
+  capital: "Tehran",
+  language: "Farsi",
+  population: 80,
+  neighbours: ["Turkey", "Russia", "Iraq", "Afghanistan", "Pakistan", "Torkamanestan"],
+
+  describe: function () {
+    return `${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}`;
+  },
+  checkIsland: function () {
+    this.isIsland = this.neighbours.length === 0 ? true : false;
+    return this.isIsland;
+  },
+};
+
+/* LECTURE: Dot vs. Bracket Notation
+1. Using the object from the previous assignment, log a string like this to the
+console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries
+and a capital called Helsinki.'
+2. Increase the country's population by two million using dot notation, and then
+decrease it by two million using brackets notation.*/
+
+console.log(
+  `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}`
+);
+myCountry.population = myCountry.population + 2;
+console.log(myCountry.population);
+myCountry["population"] = myCountry["population"] - 2;
+console.log(myCountry["population"]);
+
+/* LECTURE: Object Methods
+1. Add a method called 'describe' to the 'myCountry' object. This method
+will log a string to the console, similar to the string logged in the previous
+assignment, but this time using the 'this' keyword.
+2. Call the 'describe' method
+3. Add a method called 'checkIsland' to the 'myCountry' object. This
+method will set a new property on the object, called 'isIsland'.
+'isIsland' will be true if there are no neighbouring countries, and false if
+there are. Use the ternary operator to set the property.*/
+console.log(myCountry.describe());
+console.log(myCountry.checkIsland());
+console.log(myCountry);
+
+/* LECTURE: Iteration: The for Loop
+1. There are elections in your country! In a small town, there are only 50 voters.
+Use a for loop to simulate the 50 people voting, by logging a string like this to
+the console (for numbers 1 to 50): 'Voter number 1 is currently voting'*/
--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -93,3 +93,166 @@ console.log(yearsUntilRetirement1(1989, "Sahar"));
 console.log(yearsUntilRetirement1(1950, "Ali"));
 
 //Introduction to Arrays
+
+const friend1 = "Rezvan";
+const friend2 = "Negar";
+const friend3 = "Nasim";
+//1
+const friends = ["Rezvan", "Negar", "Nasim"];
+console.log(friends);
+//2
+const years = new Array(2021, 1989, 2011, 1800);
+console.log(years);
+
+console.log(friends[0]);
+console.log(years.length);
+console.log(friends[friends.length - 1]);
+
+friends[1] = "Farzaneh";
+console.log(friends);
+
+//friends = ["a", "b", "c"];// Error: Assignment to constant variable.
+//console.log(friends);
+const firstName = "Sahar";
+const Sahar = [firstName, "Soltanmohammadi", 2022 - 1989, "student", friends];
+console.log(Sahar);
+
+//exercise
+const calculateAge = function (birthYear) {
+  return 2022 - birthYear;
+};
+const year = [1989, 1950, 2011, 2009];
+//console.log(calculateAge(year)); //NaN
+const ageA = calculateAge(year[0]);
+const ageB = calculateAge(year[1]);
+const ageC = calculateAge(year[year.length - 1]);
+console.log(ageA, ageB, ageC);
+
+const ages = [calculateAge(year[0]), calculateAge(year[1]), calculateAge(year[year.length - 1])];
+console.log(ages);
+
+//Basic Array Operations (Methods)
+const myFriends = ["Rezvan", "Negar", "Nasim"];
+//Adding in Array
+const newFriends = myFriends.push("Farzaneh");
+console.log(myFriends);
+console.log(newFriends); //4
+
+const newFriend = myFriends.unshift("Parisa");
+console.log(myFriends);
+console.log(newFriend);
+
+//Removing from Array
+myFriends.pop();
+myFriends.shift();
+console.log(myFriends);
+const myNewFriends = myFriends.shift();
+console.log(myFriends);
+console.log(myNewFriends);
+
+console.log(myFriends.indexOf("Rezvan"));
+console.log(myFriends.indexOf("Nasim"));
+
+myFriends.push(23);
+console.log(myFriends.includes("Nasim"));
+console.log(myFriends.includes("Rezvan"));
+console.log(myFriends.includes("23")); //false
+console.log(myFriends.includes(23)); //true
+
+if (myFriends.includes("Nasim")) {
+  console.log("You have a friend called Nasim");
+}
+
+//Introduction to Objects
+const saharArray = [
+  "Sahar",
+  "Soltanmohammadi",
+  2022 - 1989,
+  "Student",
+  ["Rezvan", "Nasim", "Negar"],
+];
+console.log(saharArray);
+
+const saharObject = {
+  firstName: "Sahar",
+  lastName: "Soltanmohammadi",
+  age: 2022 - 1989,
+  job: "Student",
+  friends: ["Rezvan", "Nasim", "Negar"],
+};
+console.log(saharObject);
+
+//Dot vs. Bracket Notation
+
+console.log(saharObject.firstName);
+console.log(saharObject["firstName"]);
+console.log(saharObject.lastName);
+console.log(saharObject["lastName"]);
+
+const newName = "Name";
+console.log(saharObject["first" + newName]);
+console.log(saharObject["last" + newName]);
+
+//console.log(saharObject.'last'+newName);//Error: we cannot write dot with expression
+
+const interestedIn = prompt(
+  "What do you want to know about Sahat? Choose between firstName, lastname, age, job, friends"
+);
+console.log(interestedIn);
+//console.log(saharObject.interestedIn);//undefined
+if (saharObject[interestedIn]) {
+  console.log(saharObject[interestedIn]);
+} else {
+  console.log("Wrong request! Choose between firstName, lastname, age, job, friends ");
+}
+saharObject.location = "Finland";
+saharObject["LinkedIn"] = "@saharsolt";
+
+console.log(saharObject);
+//challenge
+//"Sahar has 3 friends, and Rezvan is her best friend."
+console.log(
+  `${saharObject["firstName"]} has ${saharObject.friends.length} friends, and ${saharObject["friends"][0]} is her best friend. `
+);
+
+//Object Methods
+const sahar = {
+  firstName: "Sahar",
+  lastName: "Soltanmohammadi",
+  birthYear: 1989,
+  job: "Student",
+  friends: ["Rezvan", "Nasim", "Negar"],
+  hasDriverLicense: false,
+
+  // calcAge: function (birthYear) {
+  //   return 2022 - birthYear;
+
+  // calcAge: function () {
+  //   console.log(this);
+  //   return 2022 - this.birthYear;
+  // }
+
+  calcAge: function () {
+    this.age = 2022 - this.birthYear;
+    return this.age;
+  },
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${
+      this.hasDriverLicense ? "a" : "no"
+    } driver's license`;
+  },
+};
+console.log(sahar.calcAge());
+//console.log(sahar["calcAge"](1989));
+console.log(sahar.age);
+console.log(sahar.age);
+console.log(sahar.age);
+
+//challenge
+//'Sahar is a 33-year old student, and he has a driver's license.'
+// console.log(
+//   `${sahar.firstName} is a ${sahar.calcAge()}-year old ${sahar.job}, and he has ${
+//     sahar.hasDriverLicense ? "a" : "no"
+//   } driver's license`
+// );
+console.log(sahar.getSummary());
