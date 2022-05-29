2919afceb7cc5beeeda95f1f6235e0861af39ffa
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-05-29 21:00:37 +0300

    Getting and setting value with DOM manipulation

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,2 +1,10 @@
 'use strict';
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🏆 Correct Number!';
+//console.log(document.querySelector('.message').textContent);
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
