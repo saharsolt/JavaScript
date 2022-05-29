a5939accfdce71374cc9997a0fb43db08c8ea6a8
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-05-29 21:29:35 +0300

    No input

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,5 +1,5 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
+/* console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🏆 Correct Number!';
 //console.log(document.querySelector('.message').textContent);
 document.querySelector('.number').textContent = 13;
@@ -7,4 +7,20 @@ document.querySelector('.score').textContent = 10;
 
 console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+console.log(document.querySelector('.guess').value); */
+
+document.querySelector('.check').addEventListener('click', function () {
+  //console.log(document.querySelector('.guess').value);
+
+  //document.querySelector('.message').textContent = '🏆 Correct Number!';
+
+  //const guess = document.querySelector('.guess').value;
+  //console.log(guess, typeof guess);
+
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+});
