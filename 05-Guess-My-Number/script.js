d53e1a02a8dd517a884e5cec9666790f407cf6b8
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-05-30 20:48:51 +0300

    Implementing highscore

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -8,8 +8,9 @@ document.querySelector('.score').textContent = 10;
 console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value); */
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -25,6 +26,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈too high';
@@ -60,8 +66,9 @@ Implement a game rest functionality, so that the player can make a new guess! He
 GOOD LUCK 😀
 */
 document.querySelector('.again').addEventListener('click', function () {
-  const secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.score').textContent = 20;
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.score').textContent = score;
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('body').style.backgroundColor = '#222';
