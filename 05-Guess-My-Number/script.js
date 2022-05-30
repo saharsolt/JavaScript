bf62d406bf1b21b30d543fbaf6e97e97f690a906
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-05-30 19:13:34 +0300

    Logic of the game

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -8,19 +8,36 @@ document.querySelector('.score').textContent = 10;
 console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value); */
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
-  //console.log(document.querySelector('.guess').value);
-
-  //document.querySelector('.message').textContent = '🏆 Correct Number!';
-
-  //const guess = document.querySelector('.guess').value;
-  //console.log(guess, typeof guess);
-
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🏆 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
