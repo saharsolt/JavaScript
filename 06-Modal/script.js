b309b609c6ed2eaf1dda6d20c4dc37d4a9495a20
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-06-02 00:48:33 +0300

    Openning and closing modal

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,10 +1,25 @@
 'use strict';
 const modal = document.querySelector('.modal');
-const overlay = document.querySelector('overlay');
+const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnOpenModal);
 
 for (let i = 0; i < btnOpenModal.length; i++) {
-  console.log(btnOpenModal[i].textContent);
+  //console.log(btnOpenModal[i].textContent);
+  btnOpenModal[i].addEventListener('click', function () {
+    console.log('button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
 }
+
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
