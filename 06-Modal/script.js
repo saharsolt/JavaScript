71d0a7e63c5fbee8e6a361573724f4fc0889b736
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-06-02 08:46:22 +0300

    Refactoring modal code

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -3,23 +3,23 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnOpenModal);
+//console.log(btnOpenModal);
+
+const closeModel = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModel = function () {
+  console.log('button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnOpenModal.length; i++) {
   //console.log(btnOpenModal[i].textContent);
-  btnOpenModal[i].addEventListener('click', function () {
-    console.log('button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnOpenModal[i].addEventListener('click', openModel);
 }
 
-btnCloseModal.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
-
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+btnCloseModal.addEventListener('click', closeModel);
+overlay.addEventListener('click', closeModel);
