f789593603978362a150ff8373c8355641235f34
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-06-17 00:41:14 +0300

    Hoisting and Variables

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -40,3 +40,38 @@ const firstName = 'Sahar';
 calcAge(1989);
 //console.log(age);     ReferenceError
 //printAge();       ReferenceError
+
+//Variables
+console.log(me); //undefined
+//console.log(job); //ReferenceError: Cannot access 'job' before initialization
+//console.log(year);
+
+var me;
+const job = 'teacher';
+let year = 1989;
+
+//Functions
+
+console.log(addDecl(2, 4)); //6
+// console.log(addExpr(2, 4)); //ReferenceError: Cannot access 'addExpr' before initialization
+// console.log(addArrow(2, 4)); //ReferenceError: Cannot access 'addExpr' before initialization
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+//var is undefined and undefined(2,4) is not a function
+// console.log(addExp(2, 4)); //addExp is not a function
+// console.log(addArrw(2, 4));
+
+var addExp = function (a, b) {
+  return a + b;
+};
+
+var addArrw = (a, b) => a + b;
