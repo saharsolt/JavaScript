152e06182c4953262ddf594ee8608540bb47e9f2
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-06-22 00:49:25 +0300

    Challenge1

--- 09-Data-Structures-Operators/challenge.js
+++ 09-Data-Structures-Operators/challenge.js
@@ -26,3 +26,53 @@ operator.
 Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
 Then, call the function again with players from game.scored
 GOOD LUCK 😀*/
+'use strict';
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const players1 = [game.players[0]];
+const players2 = [game.players[1]];
+console.log(players1);
+console.log(players2);
+const [gk, ...fieldPlayers] = game.players[0];
+console.log(gk);
+console.log(fieldPlayers);
