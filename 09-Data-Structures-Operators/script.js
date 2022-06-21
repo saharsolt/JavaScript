dd01ebd0e6774ebe9b4d58c7a7bf901c7925d6ed
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-06-21 23:45:55 +0300

    Logical Assignment Operators

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -272,3 +272,32 @@ restaurant.numGuests = 0;
 
 const guests = restaurant.numGuests ?? 40;
 console.log(guests); //0
+
+const rest1 = {
+  name: 'Rax',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'HotBurger',
+  owner: 'Masoud Shokrnezhad',
+};
+//OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// rest2.owner = rest2.owner && 'Anonymous'; //Anonymous
+// rest1.owner = rest1.owner && 'Anonymous'; //undefined
+
+rest2.owner &&= 'Anonymous';
+rest1.owner &&= 'Anonymous';
+
+console.log(rest1);
+console.log(rest2);
