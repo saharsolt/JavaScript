79696d352c1cd51ec43b9a1244079fa8b0cc508f
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-06-22 22:59:44 +0300

    Completing challenge

--- 09-Data-Structures-Operators/challenge.js
+++ 09-Data-Structures-Operators/challenge.js
@@ -69,10 +69,37 @@ const game = {
   },
 };
 
-const players1 = [game.players[0]];
-const players2 = [game.players[1]];
-console.log(players1);
-console.log(players2);
-const [gk, ...fieldPlayers] = game.players[0];
+// const players1 = game.players[0];
+// const players2 = game.players[1];
+
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+const [gk, ...fieldPlayers] = players1;
 console.log(gk);
 console.log(fieldPlayers);
+
+// const [gk2, ...fieldPlayers2] = players2;
+// console.log(gk2);
+// console.log(fieldPlayers2);
+
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+const { team1, x: draw, team2 } = game.odds;
+// const{odds : {team1,x:draw,team2}}=game;
+console.log(team1, draw, team2);
+
+const printGoals = function (...players) {
+  console.log(`${players.length} goals were scored!`);
+};
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals('Davies', 'Muller');
+printGoals(...game.scored);
+
+team1 < team2 && console.log('team1 is likely to win!');
+team2 < team1 && console.log('team2 is likely to win!');
