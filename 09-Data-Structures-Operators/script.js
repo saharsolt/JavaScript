1e80c65e50674729ff33ce1144d53c709801c15c
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-06-23 23:57:57 +0300

    Optional chaining

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -32,6 +32,7 @@ const restaurant = {
 
   //ES6 for enhanced object literals
   openingHours1,
+  openingHours: openingHours1,
 
   order(starterMenuIndex, mainMenuIndex) {
     return [this.starterMenu[starterMenuIndex], this.mainMenu[mainMenuIndex]];
@@ -326,3 +327,33 @@ for (const [i, el] of menu1.entries()) {
   //   console.log(`${item[0] + 1}: ${item[1]}`);
   console.log(`${i + 1}: ${el}`);
 }
+
+//Optional chaining
+console.log(restaurant.openingHours.mon); //undefined
+//console.log(restaurant.openingHours.mon.open);//Error
+
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+const days = ['Mon', 'Tus', 'Wed', 'Tur', 'Fri', 'Sat', 'Sun'];
+
+for (const day of days) {
+  // const open = restaurant.openingHours[day]?.open || 'closed';
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}- we are open at ${open}`);
+}
+
+//For methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist!');
+console.log(restaurant.orderRosito?.(0, 1) ?? 'Method does not exist!');
+
+//For arrays
+const users = [{ name: 'Sahar', email: 'gmail.com' }];
+//const users=[];
+console.log(users[0]?.name ?? 'user array is empty!');
+
+if (users.length > 0) console.log(users.name);
+else console.log('user array is emoty!');
