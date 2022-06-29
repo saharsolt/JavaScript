1f7ab82e5a9efaffceb94fa893d9c79d067c855d
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-06-29 21:44:09 +0300

    Challenge#4

--- 09-Data-Structures-Operators/challenge.js
+++ 09-Data-Structures-Operators/challenge.js
@@ -225,3 +225,70 @@ for (const [min, event] of gameEvents.entries()) {
   const half = min < 45 ? '[FIRST HALF]' : '[SECOND HALF]';
   console.log(`${half} ${min}: ${event}`);
 }
+
+/*
+Coding Challenge #4
+Write a program that receives a list of variable names written in underscore_case
+and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below to
+insert the elements), and conversion will happen when the button is pressed.
+Test data (pasted to textarea, including spaces):
+underscore_case
+first_name
+Some_Variable
+calculate_AGE
+delayed_departure
+Should produce this output (5 separate console.log outputs):
+underscoreCase ✅
+firstName ✅✅
+someVariable ✅✅✅
+calculateAge ✅✅✅✅
+delayedDeparture ✅✅✅✅✅
+Hints:
+§ Remember which character defines a new line in the textarea 😉
+§ The solution only needs to work for a variable made out of 2 words, like a_b
+§ Start without worrying about the ✅. Tackle that only after you have the variable
+name conversion working 😉
+§ This challenge is difficult on purpose, so start watching the solution in case
+you're stuck. Then pause and continue!
+Afterwards, test with your own test data!
+GOOD LUCK 😀
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+*/
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  console.log(document.querySelector('textarea').value);
+  const name = document.querySelector('textarea').value.split('\n');
+  console.log(name);
+  let nameArray = [];
+  for (const variable of name.values()) {
+    // const nameChanged = .indexOf(variable.indexOf('_') + 1);
+    const variableLower = variable.toLowerCase();
+    const index = variable.indexOf('_') + 1;
+    //console.log(index);
+    const nameChanged =
+      variableLower.slice(0, index) +
+      variableLower[index].toUpperCase() +
+      variableLower.slice(index + 1);
+    //console.log(nameChanged);
+    const camelcaseName = nameChanged.replace('_', '');
+    console.log(camelcaseName);
+    nameArray.push(camelcaseName);
+  }
+  //console.log(nameArray);
+  //const finalResult = nameArray.join(` ${'✅'.repeat(items.index)} \n`);
+  let i = 0;
+  let addedCheckedName = [];
+  for (const item of nameArray.values()) {
+    const newItem = item + ' ' + '✅'.repeat(i + 1);
+    console.log(newItem);
+    i++;
+    addedCheckedName.push(newItem);
+  }
+  //console.log(addedCheckedName);
+  const finalResult = addedCheckedName.join('\n');
+  console.log(finalResult);
+});
