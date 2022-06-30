b402e7cb39b67bdff16232ed1a7b8e7e1417c0dd
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-06-30 23:59:52 +0300

    Value Vs. refrence

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -22,3 +22,33 @@ createBooking('LH241');
 createBooking('LH849', 2, 800);
 createBooking('LH377', 3);
 createBooking('LH347', undefined, 900);
+
+const flight = 'LH843';
+const sahar = {
+  name: 'Sahar Soltanmohammadi',
+  passport: 8378738675,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH635';
+  passenger.name = 'Ms.' + passenger.name;
+
+  if (passenger.passport === 8378738675) {
+    alert('Checked in!');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+checkIn(flight, sahar);
+console.log(flight); //LH843
+console.log(sahar); //{name: 'Ms.Sahar Soltanmohammadi', passport: 8378738675}
+
+//Is the same as doing ...
+const flightNum = flight;
+const passenger = sahar;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+newPassport(sahar);
+checkIn(flight, sahar);
