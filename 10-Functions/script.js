ba4ebdcc8598facb6839c8a8badc78a637c5619b
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-06-30 23:35:53 +0300

    Default parameters

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  //ES5
+  //numPassengers = numPassengers || 1;
+  //price = price || 199;
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  bookings.push(booking);
+  console.log(bookings);
+};
+createBooking('LH241');
+createBooking('LH849', 2, 800);
+createBooking('LH377', 3);
+createBooking('LH347', undefined, 900);
