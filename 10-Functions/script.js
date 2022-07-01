449eb3788f2c8327c3f6f9c66807cfa524b90ea9
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-07-01 19:05:14 +0300

    Higher-order functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -40,15 +40,41 @@ const checkIn = function (flightNum, passenger) {
   }
 };
 checkIn(flight, sahar);
-console.log(flight); //LH843
-console.log(sahar); //{name: 'Ms.Sahar Soltanmohammadi', passport: 8378738675}
+// console.log(flight); //LH843
+// console.log(sahar); //{name: 'Ms.Sahar Soltanmohammadi', passport: 8378738675}
 
 //Is the same as doing ...
-const flightNum = flight;
-const passenger = sahar;
+// const flightNum = flight;
+// const passenger = sahar;
 
 const newPassport = function (person) {
   person.passport = Math.trunc(Math.random() * 100000000);
 };
 newPassport(sahar);
 checkIn(flight, sahar);
+
+//First-class VS. Higher-order functions
+const oneWord = function (str) {
+  return str.replaceAll(' ', '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+//Higher-order function: transformer
+//Call back function: fn
+const transformer = function (str, fn) {
+  console.log(`The original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+//JS uses callback all the time
+const high5 = function (str) {
+  console.log('😜');
+};
+document.body.addEventListener('click', high5);
+['Sahar', 'Masoud', 'Neda', 'Meghdad'].forEach(high5);
