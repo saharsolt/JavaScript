44617963113b2f1cdc831e2a37c68ea789fe2a10
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-07-08 23:54:07 +0300

    Splice and Reverse method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -83,3 +83,17 @@ console.log(arr.slice(2, 4));
 console.log(arr.slice(1, -1));
 console.log(arr.slice());
 console.log([...arr]);
+
+//Splice method
+// console.log(arr.splice(2)); //['c', 'd', 'e']
+// console.log(arr); //['a', 'b']
+arr.splice(-1);
+console.log(arr); //['a', 'b', 'c', 'd']
+arr.splice(1, 2);
+console.log(arr); //['a', 'd']
+
+//Reverse method
+//arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2); //['f', 'g', 'h', 'i', 'j']
