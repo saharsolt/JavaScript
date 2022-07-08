876ecbbc072418a53f0e4a7faafd044b3261a0dd
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-07-08 20:21:31 +0300

    Slice method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -74,3 +74,12 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+//Slice method
+const arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(arr.slice(2));
+console.log(arr.slice(-2));
+console.log(arr.slice(-2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(1, -1));
+console.log(arr.slice());
+console.log([...arr]);
