ed99a8a4ce20e91ffdc9071a75770543edbc68eb
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-07-14 23:12:08 +0300

    Summary-in bankist

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -75,7 +75,7 @@ const displayedMovements = function (movements) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `<div class="movements__row">
   <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-  <div class="movements__value">${mov}</div>
+  <div class="movements__value">${mov}€</div>
 </div>`;
     containerMovements.insertAdjacentHTML('afterbegin', html);
     //containerMovements.insertAdjacentHTML('beforeend', html);
@@ -89,6 +89,14 @@ const calcDisplayBalance = function (movements) {
   labelBalance.textContent = `${balance} €`;
 };
 calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+  const input = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = input;
+};
+calcDisplaySummary(account1.movements);
 //console.log(containerMovements.innerHTML);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
