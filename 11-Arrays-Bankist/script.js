5ef444643e6d2ed5b1f5eade8343beac07140c0b
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-07-16 23:23:38 +0300

    every method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -413,7 +413,18 @@ console.log(account); //{owner: 'Sahar Soltan Mohammadi', movements: Array(5), i
 //some method
 //Equality
 console.log(movements.includes(-130)); //true
-//Condition
+
+//some: Condition
 console.log(movements.some(mov => mov === -130));
 const anyDeposite = movements.some(mov => mov > 0);
 console.log(anyDeposite); //true
+
+//every
+console.log(movements.every(mov => mov > 0)); //false
+console.log(account4.movements.every(mov => mov > 0)); //true
+
+//Separate callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
