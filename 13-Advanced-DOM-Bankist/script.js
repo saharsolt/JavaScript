0100c8ba7ba45be3531b8e07eb0b4d81c2882158
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-08-03 17:46:48 +0300

    Creating, inserting, and deleting elements

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -40,7 +40,7 @@ console.log(document.documentElement);
 console.log(document.body);
 console.log(document.head);
 
-document.querySelector('.header');
+const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
 console.log(allSections);
 
@@ -49,3 +49,26 @@ const allButtons = document.getElementsByTagName('button');
 console.log(allButtons);
 
 console.log(document.getElementsByClassName('btn'));
+
+//Creating and inserting elements
+//.insertAdjacentHTML
+const message = document.createElement('div');
+message.classList.add('cookie-message'); //in oreder to show the element on DOM
+
+//message.textContent ='We used cookies for improved functionality and analytics';
+message.innerHTML =
+  'We used cookied for improved functionality and analytics. <button class ="btn btn--close--cookie"> Got it! </button>';
+//header.prepend(message); //to put the element in header element
+header.append(message);
+//header.append(message.cloneNode(true)); //copy the element and all of its children
+
+//header.before(message);
+//header.after(message);
+
+//Delete elements
+document
+  .querySelector('.btn--close--cookie')
+  .addEventListener('click', function () {
+    //message.remove();
+    message.parentElement.removeChild(message);
+  });
