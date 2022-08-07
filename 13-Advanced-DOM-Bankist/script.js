c301f5fb24529a534ab2149711431fbee5dd537e
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-08-07 18:44:27 +0300

    Sticky navigations

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -168,6 +168,17 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 //   }
 //   logo.style.opacity = 1;
 // });
+
+//Implementing a sticky navigation
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+
+window.addEventListener('scroll', function () {
+  console.log(window.scrollY);
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
+
 //////////////////////////////////////////
 //Selecting elements
 /*
