30da9c208b3d939e67cf53e6794078dfba3af0a0
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-08-07 19:51:10 +0300

    Intersection Observer API

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -170,15 +170,28 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 // });
 
 //Implementing a sticky navigation
-const initialCoords = section1.getBoundingClientRect();
-console.log(initialCoords);
+// const initialCoords = section1.getBoundingClientRect();
+// console.log(initialCoords);
 
-window.addEventListener('scroll', function () {
-  console.log(window.scrollY);
-  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
-  else nav.classList.remove('sticky');
-});
+// window.addEventListener('scroll', function () {
+//   console.log(window.scrollY);
+//   if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+//   else nav.classList.remove('sticky');
+// });
 
+//Sticky navigation: Intersection Observer API
+const obsCallback = function (entries, observer) {
+  entries.forEach(entries => console.log(entries));
+};
+
+const obsOptions = {
+  root: null,
+  // threshold: 0.1,
+  threshold: [0, 0.2],
+};
+
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+observer.observe(section1);
 //////////////////////////////////////////
 //Selecting elements
 /*
