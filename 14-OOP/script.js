aa925cf2966429000837ede6965e1bce7d6e42be
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-08-13 18:57:24 +0300

    Prototypes

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -21,3 +21,25 @@ console.log(sahar);
 //4. Constructor function automatically return {}
 
 console.log(jonas instanceof Person); //true
+
+//ProtoTypes
+console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+  console.log(2022 - this.birthYear);
+};
+jonas.calcAge();
+sahar.calcAge();
+
+console.log(jonas.__proto__);
+console.log(Person.prototype);
+console.log(jonas.__proto__ === Person.prototype); //true
+
+console.log(Person.prototype.isPrototypeOf(sahar)); //true
+console.log(Person.prototype.isPrototypeOf(Person)); //false
+//.prototypeOfLinkedObjects
+
+Person.prototype.species = 'Homo sapines';
+console.log(jonas.species, sahar.species);
+
+console.log(jonas.hasOwnProperty('firstName')); //true
+console.log(jonas.hasOwnProperty('species')); //false
