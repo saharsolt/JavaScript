885baf2cbec2a43ab454d9194acff67a61c8c3fa
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-08-13 18:00:52 +0300

    Constructor functions and the new operator

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,1 +1,23 @@
 'use strict';
+const Person = function (firstName, birthYear) {
+  //console.log(this);
+  //Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  //console.log(this);
+  //Never do this
+  //   this.calcAge = function () {
+  //     console.log(2022 - this.birthYear);
+  //   };
+};
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+const sahar = new Person('Sahar', 1989);
+console.log(sahar);
+
+//1. New {} is created
+//2. Function is called, this={}
+//3. {} linked to prototype
+//4. Constructor function automatically return {}
+
+console.log(jonas instanceof Person); //true
