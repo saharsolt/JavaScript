fe5a2686d68f40df903557f7d7f8eedf55d612e0
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-08-14 23:44:36 +0300

    ES6 Classes

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -66,3 +66,36 @@ console.log(arr.unique()); //[3, 5, 6, 7, 1, 8]
 
 const h1 = document.querySelector('h1'); //HTMLHeadingElement prototype
 console.dir(x => x + 1); //Function prototype
+
+//Class expression
+// const PersonCl = class{};
+
+//Class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+  //Methods will be added to .prototype property
+  calcAge() {
+    console.log(2022 - this.birthYear);
+  }
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1987);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+
+jessica.greet();
+
+//1. Classes are not hoisted
+//2. Classes are first-class citizens
+//3. Classes are executed in strict mode
