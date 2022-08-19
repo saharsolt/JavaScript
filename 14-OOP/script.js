5abb87f04d697fbef15fb0471e872bf1fcc78d51
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-08-19 23:10:36 +0300

    Encapsulation-Protected properties and methods

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -256,23 +256,28 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
+    //Protected property
+    this._pin = pin;
+    this._movements = [];
     this.locale = navigator.language;
     console.log(`Thanks for opening an account, ${this.owner}`);
   }
   //Public interface
+  getMovements() {
+    return this._movements;
+  }
+
   deposite(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
   withdraw(val) {
     this.deposite(-val);
   }
-  approveLoan(val) {
+  _approveLoan(val) {
     return true;
   }
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposite(val);
       console.log('Loan approved!');
     }
@@ -280,10 +285,11 @@ class Account {
 }
 
 const acc1 = new Account('Jonas', 'EUR', 1111);
-// acc1.movements.push(250);
-// acc1.movements.push(-140);
+// acc1._movements.push(250);//Not accessible from outside of class
+// acc1._movements.push(-140);
 acc1.deposite(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
 acc1.approveLoan(1000);
+console.log(acc1.getMovements());
 console.log(acc1);
