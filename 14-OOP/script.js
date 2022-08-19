d9d6e0dca71a2b827e4bb1aed8786e09e17c8ab8
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-08-19 22:54:53 +0300

    Another example

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -250,3 +250,40 @@ const joy = Object.create(StudentProto);
 joy.init('Joy Keli', 1995, 'Computer Science');
 joy.introduce();
 joy.calcAge();
+
+//Another class example
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+    console.log(`Thanks for opening an account, ${this.owner}`);
+  }
+  //Public interface
+  deposite(val) {
+    this.movements.push(val);
+  }
+  withdraw(val) {
+    this.deposite(-val);
+  }
+  approveLoan(val) {
+    return true;
+  }
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposite(val);
+      console.log('Loan approved!');
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposite(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+console.log(acc1);
