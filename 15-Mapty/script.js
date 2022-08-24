f79ee474cec24abd51bc09d410f7b0a81bb7cb4e
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-08-24 20:40:03 +0300

    Map marker

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -30,10 +30,11 @@ if (navigator.geolocation) {
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng]).addTo(map).bindPopup('Workout!').openPopup();
+      });
     },
     function () {
       alert('Could not get your position!');
