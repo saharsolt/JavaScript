0ceb99d95554d25aba16cb6a24c6411f1d630848
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-08-26 23:13:52 +0300

    Creating parent and child classes

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -77,6 +77,48 @@ const inputElevation = document.querySelector('.form__input--elevation');
 //   inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
 // });
 
+class Workout {
+  date = new Date();
+  id = (Date.now + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    //this.date = ...
+    //this.id = ...
+    this.coords = coords; // [lat,lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    //km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.pace;
+  }
+}
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycle1 = new Cycling([39, -12], 27, 95, 523);
+// console.log(run1, cycle1);
+
+///////////////////////////////////////
+//Application architecture
 //Refactoring the project
 class App {
   #map;
