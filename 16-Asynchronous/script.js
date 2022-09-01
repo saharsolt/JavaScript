eb40313c53b8683a61375b5e6e0e97e1556f0662
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-09-01 23:57:53 +0300

    Our First AJAX call

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -4,3 +4,11 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
+request.send();
+console.log(request.responseText);
+
+request.addEventListener('load', function () {
+  console.log(this.responseText);
+});
