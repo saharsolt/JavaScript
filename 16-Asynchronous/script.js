78effcb7f13a16a4c1be24385129502666d056f8
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-09-04 22:44:53 +0300

    Callback Hell

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -39,6 +39,78 @@ const getCountryData = function (country) {
   });
 };
 
-getCountryData('portugal');
-getCountryData('usa');
-getCountryData('iran');
+// getCountryData('portugal');
+// getCountryData('usa');
+// getCountryData('iran');
+
+////////////////////////////////////////////
+const renderCountry = function (data, className = '') {
+  const html = `<article class="country ${className}">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${
+        Object.entries(data.languages)[0][1]
+      }</p>
+      <p class="country__row"><span>💰</span>${
+        Object.entries(data.currencies)[0][1]['name']
+      }</p>
+    </div>
+  </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryDataAndNeighbours = function (country) {
+  //AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  console.log(request.responseText);
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    //Render country 1
+    renderCountry(data);
+
+    //Get neighbour country(2)
+    const neighbour = data.borders?.[0];
+    //AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+    console.log(request2.responseText);
+
+    request2.addEventListener('load', function () {
+      //console.log(this.responseText);
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+// getCountryDataAndNeighbours('portugal');
+// getCountryDataAndNeighbours('usa');
+getCountryDataAndNeighbours('iran');
+
+//For example of call back hell
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 seconds passed');
+      setTimeout(() => {
+        console.log('4 seconds passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
