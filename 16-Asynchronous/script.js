add5f1c2c648b1b5b9c0b879e2670b00b4b12028
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-09-06 23:58:23 +0300

    Throwing errors manually

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -140,19 +140,27 @@ setTimeout(() => {
 //     });
 // };
 
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg}(${response.status})`);
+    return response.json();
+  });
+};
+
 const getCountryData = function (country) {
   //Country1
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(response => response.json())
+  getJSON(`https://restcountries.com/v3.1/name/${country}`, 'Country not found')
     .then(data => {
       renderCountry(data[0]);
       const neighbour = data[0].borders[0];
 
-      if (!neighbour) return;
+      if (!neighbour) throw new Error('No neighbour found!');
       //Country2
-      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+      return getJSON(
+        `https://restcountries.com/v3.1/alpha/${neighbour}`,
+        'Country not found'
+      );
     })
-    .then(response => response.json())
     .then(data => renderCountry(data[0], 'neighbour'))
     .catch(error => {
       console.error(`${error} 💥💥💥💥!`);
@@ -160,9 +168,42 @@ const getCountryData = function (country) {
     })
     .finally(() => (countriesContainer.style.opacity = 1));
 };
+
+// const getCountryData = function (country) {
+//   //Country1
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(response => {
+//       console.log(response);
+
+//       if (!response.ok)
+//         throw new Error(`Country not found (${response.status})`);
+//       return response.json();
+//     })
+//     .then(data => {
+//       renderCountry(data[0]);
+//       // const neighbour = data[0].borders[0];
+//       const neighbour = 'fdsdggh';
+
+//       if (!neighbour) return;
+//       //Country2
+//       return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     })
+//     .then(response => {
+//       if (!response.ok)
+//         throw new Error(`Country not found (${response.status})`);
+//       return response.json();
+//     })
+//     .then(data => renderCountry(data[0], 'neighbour'))
+//     .catch(error => {
+//       console.error(`${error} 💥💥💥💥!`);
+//       renderError(`Something went wrong 💥💥💥${error.message}. Try again!`);
+//     })
+//     .finally(() => (countriesContainer.style.opacity = 1));
+// };
 btn.addEventListener('click', function () {
   getCountryData('portugal');
   // getCountryData('iran');
 });
 
-getCountryData('fdsdggh');
+// getCountryData('fdsdggh');
+getCountryData('australia');
