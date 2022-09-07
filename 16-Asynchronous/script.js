4b3eb61efbb695cc86bce59875af5f8b68e5424a
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-09-07 23:10:40 +0300

    Event loop

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -207,3 +207,16 @@ btn.addEventListener('click', function () {
 
 // getCountryData('fdsdggh');
 //getCountryData('australia');
+
+//Event loop
+console.log('Test start!');
+setTimeout(() => {
+  console.log('0 sec timer');
+}, 0);
+Promise.resolve('Resolve promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolve promise 2').then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+});
+console.log('Test end!');
