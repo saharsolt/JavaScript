df735bba0a3ba486181fdc9180616282a60d7b4f
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-09-14 20:10:54 +0300

    Other promise combinators

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -426,3 +426,47 @@ const get3Countries = async function (c1, c2, c3) {
 };
 
 get3Countries('Portugal', 'iran', 'france');
+
+//Promise.race
+
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.com/v3.1/name/italy`),
+    getJSON(`https://restcountries.com/v3.1/name/iran`),
+    getJSON(`https://restcountries.com/v3.1/name/portugal`),
+  ]);
+  console.log(res[0].name);
+})();
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error('The process took too long'));
+    }, sec * 1000);
+  });
+};
+Promise.race([getJSON(`https://restcountries.com/v3.1/name/iran`), timeout(1)])
+  .then(res => console.log(res[0].name))
+  .catch(err => console.error(err));
+
+//Promise.allSettled
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('Another Success'),
+]).then(res => console.log(res));
+
+Promise.all([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('Another Success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+
+//Promise.any [ES2021]
+Promise.any([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('Another Success'),
+]).then(res => console.log(res));
