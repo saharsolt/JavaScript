31e91159ec4d7af03743c35e2fcb8d59b46972fd
saharsolt <sahar.soltanmohammadi@metropolia.fi>
2022-09-16 00:21:12 +0300

    Exporting and importing in ES6 module

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -1,7 +1,29 @@
 //Importing module
-import { addToCart, totalPrice, totalQuantity } from './shoppingCard.js';
+// import {
+//   addToCart,
+//   totalPrice as price,
+//   totalQuantity,
+// } from './shoppingCard.js';
 console.log('Importing modules');
 
 // console.log(shippingCard);
-addToCart('bread', 5);
-console.log(totalPrice, totalQuantity);
+// addToCart('bread', 5);
+// console.log(price, tq);
+
+// import * as ShoppingCart from './shoppingCard.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+// import add,{
+//       addToCart,
+//       totalPrice as price,
+//       totalQuantity,
+//     } from './shoppingCard.js';
+//     console.log(price);
+
+import add, { cart } from './shoppingCard.js';
+add('pizza', 2);
+add('bread', 5);
+add('pizza', 2);
+add('pasta', 1);
+console.log(cart);
--- 17-Modern-JS-Modules-Tooling/shoppingCard.js
+++ 17-Modern-JS-Modules-Tooling/shoppingCard.js
@@ -2,14 +2,21 @@
 console.log('Exporting modules');
 
 const shippingCard = 10;
-const cart = [];
+export const cart = [];
 
 export const addToCart = function (product, quality) {
   cart.push({ product, quality });
-  console.log(`${product} has ${quality}`);
+  console.log(`${quality} ${product} added to cart!`);
 };
 
 const totalPrice = 237;
 const totalQuantity = 23;
 
-export { totalPrice, totalQuantity };
+export { totalPrice, totalQuantity as tq };
+
+export default function (product, quality) {
+  cart.push({ product, quality });
+  console.log(`${quality} ${product} added to cart!`);
+}
+
+console.log(cart);
