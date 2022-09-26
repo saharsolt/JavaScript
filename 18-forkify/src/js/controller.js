940a635e0a66e46ec59d342e03f3b61fab921fb0
Sahar Soltanmohammadi <saharsoltanmohammadi@Sahars-Air.home>
2022-09-26 21:02:28 +0300

    Loading a recipe from API

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -1,4 +1,4 @@
-const recipeContainer = document.querySelector('.recipe');
+const recipeContainer = document.querySelector(".recipe");
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -11,3 +11,33 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      "https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza"
+      // "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
+    );
+    const data = await res.json();
+    console.log(res, data);
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      cookingTime: recipe.cooking_time,
+      imageUrl: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceUrl: recipe.source_url,
+      title: recipe.title,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
