36bfd301be0391d888ddaf1abfef50dece741d1e
Sahar Soltanmohammadi <sahar.solt@gmail.com>
2022-10-10 22:03:52 +0300

    Documentation

--- 18-forkify/src/js/views/view.js
+++ 18-forkify/src/js/views/view.js
@@ -4,6 +4,15 @@ import icons from "url:../../img/icons.svg"; //parcel v2
 export default class View {
   _data;
 
+  /**
+   * Render the received object to the DOM
+   * @param {object | object[]} data the data to be rendered (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render= false
+   * @this {object} View instance
+   * @author Sahar
+   * @Todo Finish implementation
+   */
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
@@ -17,6 +26,10 @@ export default class View {
     this._parentElement.insertAdjacentHTML("afterbegin", markup);
   }
 
+  /**
+   *
+   * @param {object} data  the data to be updated
+   */
   update(data) {
     this._data = data;
     console.log(this._data);
